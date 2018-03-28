002e44bcd344d2481faf8ad9a2b2e334995b16bb
takashi16 <myfree219@gmail.com>
2018-03-28 06:26:17 +0000

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180325064023) do
+ActiveRecord::Schema.define(version: 20180328062408) do
 
   create_table "books", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
     t.text "body"
-    t.integer "user_id"
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.string "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.integer "user_id"
   end
 
